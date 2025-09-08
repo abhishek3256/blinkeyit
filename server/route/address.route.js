4bb64510f8e45dc85f273941af6baaab1c3e626c
abhishek3256 <trexroy16@gmail.com>
2025-09-08 15:21:24 +0530

    8sept

--- server/route/address.route.js
+++ server/route/address.route.js
@@ -1,7 +1,6 @@
 import { Router } from 'express'
 import auth from '../middleware/auth.js'
 import { addAddressController, deleteAddresscontroller, getAddressController, updateAddressController } from '../controllers/address.controller.js'
-
 const addressRouter = Router()
 addressRouter.post('/create',auth,addAddressController)
 addressRouter.get("/get",auth,getAddressController)
