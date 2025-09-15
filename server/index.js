9815e21e5afeafc815bc5504ee202b426be49e38
abhishek3256 <trexroy16@gmail.com>
2025-09-15 13:22:04 +0530

    yesdomaincookie

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.use(cors({
             process.env.FRONTEND_URL,
             'http://localhost:3000',
             'http://localhost:5173',
-            'https://blinkeyit.vercel.app',
+            'https://blinkeyit-42rn.vercel.app',
             'https://blinkeyit-client.vercel.app'
         ];
         
