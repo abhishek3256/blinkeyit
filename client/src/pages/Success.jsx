58016debe15fd9f5fd10554c85a333dae9d1f594
abhishek3256 <trexroy16@gmail.com>
2025-11-24 23:42:11 +0530

    yes

--- client/src/pages/Success.jsx
+++ client/src/pages/Success.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
-import { Link, useLocation } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 
 const Success = () => {
   const location = useLocation()
-    
-    console.log("location",)  
+  const navigate = useNavigate()
+
+  console.log("location", location)
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigate("/")
+    }, 3000)
+  }, [navigate])
+
   return (
     <div className='m-2 w-full max-w-md bg-green-200 p-4 py-5 rounded mx-auto flex flex-col justify-center items-center gap-5'>
-        <p className='text-green-800 font-bold text-lg text-center'>{Boolean(location?.state?.text) ? location?.state?.text : "Payment" } Successfully</p>
-        <Link to="/" className="border border-green-900 text-green-900 hover:bg-green-900 hover:text-white transition-all px-4 py-1">Go To Home</Link>
+      <p className='text-green-800 font-bold text-lg text-center'>{Boolean(location?.state?.text) ? location?.state?.text : "Payment"} Successfully</p>
+      <Link to="/" className="border border-green-900 text-green-900 hover:bg-green-900 hover:text-white transition-all px-4 py-1">Go To Home</Link>
     </div>
   )
 }
